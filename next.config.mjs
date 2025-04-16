71d1c9772872922e9bbb9bf6a8a39e5a8ff43df8
khan <sg.limkhan@gmail.com>
2025-04-16 22:37:54 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
