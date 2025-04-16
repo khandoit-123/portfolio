768fd81dfbd4dacb1a0b5c5db11e858648e1eb6a
khan <sg.limkhan@gmail.com>
2025-04-16 22:48:53 +0800

    'huh'

--- src/app/AboutMe.js
+++ src/app/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () =>{
                 <div className="sm:w-1/2 border m-2 bg-black opacity-75 p-5">
                     <h2>Education</h2>
                     <div className="flex">
-                        <img src="/portfolio/pics/ns.png" width="100" height="100" alt="company" className="object-contain" />
+                        <Image src='/pics/tjc.png' width={100} height={100} alt="school" className="object-contain"></Image>
                         <div className=" p-5 justify-center text-xl"> 
                             <p className="leading-relaxed">I graduated from Temasek Junior College with a subject combination of PCME, (AAA/B) achieving a total of 88.75 RP.
                                 I am also a Peer Support Leader, where I promoted student well-being. Additionally participating in two Australian Mathematics Competitions.
@@ -23,7 +23,7 @@ const AboutMe = () =>{
                 <div className="sm:w-1/2 border m-2 bg-black opacity-75 p-5">
                     <h2>Experiences</h2>
                     <div className="flex">
-                        <img src='/pics/ns.png' width={100} height={100} alt="company" className="object-contain"></img>
+                        <Image src='/pics/ns.png' width={100} height={100} alt="school" className="object-contain"></Image>
                         <div className=" p-5 justify-center text-xl"> 
                             <p className="leading-relaxed">In my time in the army, I learnt many practical skills such as Adobe Photoshop and Premier Pro.
                                 I also acted as the primary point of contact between different branches liaising with others to secure coverage for events and content for 
