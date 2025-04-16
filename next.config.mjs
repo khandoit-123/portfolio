a73b570561649ae7e1c529ec7bc619f0a8119b77
khan <sg.limkhan@gmail.com>
2025-04-16 22:33:59 +0800

    pic fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: isProd ? '/portfolio' : '',
-    assetPrefix: isProd ? '/portfolio/' : '',
 };
 
 export default nextConfig;
